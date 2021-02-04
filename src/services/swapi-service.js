ade322b69db356cdc968e5aea2fae7206038fdd2
technoway10 <dcac7100@gmail.com>
2021-02-05 01:26:42 +0300

    базовая структура проекта

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,48 +1,41 @@
 export default class SwapiService {
 
-    _apiBase = 'https://swapi.dev/api';
+  _apiBase = 'https://swapi.co/api';
 
-    async getResource(url) {
-        const res = await fetch(`${this._apiBase}${url}`);
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
 
-        if (!res.ok) {
-            throw new Error(`Coud not fetch ${url}` +
-                `, recieved ${res.status}` )
-        }
-        return await res.json();
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}` +
+        `, received ${res.status}`)
     }
+    return await res.json();
+  }
 
-    async getAllPeople() {
-        const res = await this.getResource(`/people/`);
-        return res.results;
-    };
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  }
 
-    getPerson(id) {
-        return this.getResource(`/people/${id}/`);
-    };
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  }
 
-    async getAllPlanets() {
-        const res = await this.getResource(`/planets/`);
-        return res.results;
-    };
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+  }
 
-    getPlanet(id) {
-        return this.getResource(`/planet/${id}/`);
-    };
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}/`);
+  }
 
-    async getAllStarships() {
-        const res = await this.getResource(`/starships/`);
-        return res.results;
-    };
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+  }
 
-    getStarship(id) {
-        return this.getResource(`/starships/${id}/`);
-    };
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`);
+  }
 }
-
-const swapi = new SwapiService();
-
-swapi.getPerson(3).then((p) => {
-    console.log(p.name)
-});
-

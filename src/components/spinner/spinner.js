7872773c01d0e66a2a5c575803a12582f214abc6
technoway10 <dcac7100@gmail.com>
2021-02-08 00:50:03 +0300

    11. Логика индикатора загрузки

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-hhrkw7e9osh">
+            <div className="ldio-waf4i29kar">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+   // <style type="text/css"></style>
+    );
+};
+
+export default Spinner;
